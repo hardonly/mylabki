9fd4bd221c1cdf40a509ad70b518e11edde8b246
hardonly <aleksej.ka41@yandex.by>
2018-10-10 12:22:31 +0300

    added 4th and 5th lab

---
+++ lab_4/lab_4/Program.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node()
+        {
+            data = 0;
+            next = null;
+        }
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    public class Stack
+    {
+        public int counter = 0;
+        public Stack()
+        {
+            Begin = null;
+        }
+        public int Quantity()
+        {
+            return counter;
+        }
+        public bool IsEmpty()
+        {
+            return Begin == null;
+        }
+        public void Push(int data)
+        {
+            if (IsEmpty())
+            {
+                Begin = new Node(data);
+                counter++;
+                Console.WriteLine("Added element {0}. Total elements {1}", Begin.data, counter);
+            }
+            else
+            {
+                Node newNode = new Node(data);
+                newNode.next = Begin;
+                Begin = newNode;
+                counter++;
+                Console.WriteLine("Added element {0}. Total elements {1}", Begin.data, counter);
+            }
+        }
+        public int Pop()
+        {
+            if (!IsEmpty())
+            {
+                Node tmp = Begin;
+                int a = tmp.data;
+                Begin = tmp.next;
+                tmp.next = null;
+                counter--;
+                Console.WriteLine("Extracted element {0}. Total elements {1}", a, counter);
+                return a;
+            }
+            else
+            {
+                throw new Exception("Stack is already empty.");
+            }
+        }
+        public Node Begin { get; private set; }
+        public static class MathOperations
+        {
+            static int min = 0;
+            static int max = 0;
+            public static void MaxAndMin(int[] array)
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > max)
+                    {
+                        max = array[i];
+                    }
+                    if (array[i] < min)
+                    {
+                        min = array[i];
+                    }
+                }
+                Console.WriteLine($"\n\nMax={max}");
+                Console.WriteLine($"\nMin={min}");
+            }
+            public static int HowMuchelements(Stack s)
+            {
+                return s.Quantity();
+            }
+        }
+        public class Owner
+        {
+            public int id;
+            public string name;
+            public string organization;
+            public Owner()
+            {
+                id = 27012000;
+                name = "Egor";
+                organization = "BelSTU";
+            }
+        }
+        public class Date
+        {
+            public string data;
+            public Date()
+            {
+                data = "03 oktober 2018";
+            }
+        }
+        public static int operator +(Stack s1, int a)
+        {
+            s1.Push(a);
+            return a;
+        }
+        public static Stack operator --(Stack s1)
+        {
+            s1.Pop();
+            return s1;
+        }
+        public static bool operator true(Stack s1)
+        {
+            return s1.IsEmpty();
+        }
+        public static bool operator false(Stack s1)
+        {
+            return s1.IsEmpty();
+        }
+        public static bool operator >(Stack s1, Stack s2)
+        {
+            if (s1.IsEmpty())
+            {
+                Console.WriteLine("Stack is empty.");
+                return false;
+            }
+            int[] elements = new int[s1.Quantity()];
+            int howMuchElements = s1.Quantity();
+            for (int i = 0; i < howMuchElements; i++)
+            {
+                elements[i] = s1.Pop();
+            }
+            for (int i = 0; i < howMuchElements; i++)
+            {
+                s1.Push(elements[howMuchElements - i - 1]);
+            }
+            Array.Sort(elements);
+            for (int i = 0; i < howMuchElements; i++)
+            {
+                s2.Push(elements[i]);
+            }
+            return true;
+        }
+        public static bool operator <(Stack s1, Stack s2)
+        {
+            return false;
+        }
+    }
+    public static class StackExtansion
+    {
+        public static int Count(this Stack s)
+        {
+            int count = 0;
+            if (s.IsEmpty())
+            {
+                return count;
+            }
+            int length = s.Quantity();
+            int[] elements = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                elements[i] = s.Pop();
+                if (elements[i] >= 10 && elements[i] <= 99)
+                {
+                    count++;
+                }
+            }
+            for (int i = 0; i < length; i++)
+            {
+                s.Push(elements[length - i - 1]);
+            }
+            return count;
+        }
+        public static int Average(this Stack s)
+        {
+            int average = -1;
+            if (s.IsEmpty())
+            {
+                return average;
+            }
+            int length = s.Quantity();
+            int[] elements = new int[length];
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < length; i++)
+            {
+                elements[i] = s.Pop();
+            }
+            for (int i = 0; i < length; i++)
+            {
+                s.Push(elements[length - i - 1]);
+            }
+            for (int i = 0; i < length; i++)
+            {
+                if (min == 0 || min > elements[i])
+                {
+                    min = elements[i];
+                }
+                if (max == 0 || max < elements[i])
+                {
+                    max = elements[i];
+                }
+            }
+            int idealAverage = (max - min) / 2;
+            for (int i = 0; i < idealAverage - 1; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (elements[j] == idealAverage + i)
+                    {
+                        average = idealAverage + i;
+                        return average;
+                    }
+                    if (elements[j] == idealAverage - i)
+                    {
+                        average = idealAverage - i;
+                        return average;
+                    }
+                }
+            }
+            return average;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+            int newElem = s1 + 7;
+            newElem = s1 + 9;
+            newElem = s1 + 65;
+            newElem = s1 + 67;
+            newElem = s1 + 4;
+            s1--;
+            s1--;
+            if (s1)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Stack s2 = new Stack();
+            bool result = s1 > s2;
+
+            Console.WriteLine(s1.Count());
+
+            Console.WriteLine("Average element of stack is {0}", s1.Average());
+        }
+    }
+}
---
+++ lab_5/lab_5/Program.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5
+{
+    abstract class GeneralСharacteristics
+    {
+        public string Name { get; set; }
+        public int Area { get; set; }
+        /*public GeneralСharacteristics()
+        {
+            Console.Write("Enter name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter area: ");
+            Area = Convert.ToInt32(Console.ReadLine());
+        }*/
+    }
+
+    class Earth : GeneralСharacteristics
+    {
+        public string relief;
+        public string soil;
+        public Earth()
+        {
+            Console.WriteLine("\nCreating earth...\n");
+            Console.Write("Enter relief: ");
+            relief = Console.ReadLine();
+            Console.Write("Enter soil: ");
+            soil = Console.ReadLine();
+            Console.Write("\nCreated: ");
+            Info();
+        }
+        public void Info()
+        {
+            Console.WriteLine("Earth");
+            Console.WriteLine("relief - " + relief);
+            Console.WriteLine("soil - " + soil);
+        }
+        public override string ToString()
+        {
+            return "\nEarth relief - " + relief + ", soil - " + soil;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            Earth a = (Earth)obj;
+            if (relief == a.relief && soil == a.soil)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public new object GetType()
+        {
+            if (this is Earth)
+            {
+                return typeof(Earth);
+            }
+            if (this is Continent)
+            {
+                return typeof(Continent);
+            }
+            if (this is Island)
+            {
+                return typeof(Island);
+            }
+            if (this is State)
+            {
+                return typeof(State);
+            }
+            return typeof(object);
+        }
+    }
+
+    class Water : GeneralСharacteristics
+    {
+        public bool salinity;
+        public Water()
+        {
+            Console.WriteLine("\nCreating water...\n");
+            Console.Write("Enter salinity of water:\n1 - true\n2 - false");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 2)
+            {
+                salinity = false;
+            }
+            else
+            {
+                salinity = true;
+            }
+            Console.Write("\nCreated: ");
+            Info();
+        }
+        public void Info()
+        {
+            Console.WriteLine("Water");
+            Console.WriteLine("salinity - " + salinity);
+        }
+    }
+
+    class Continent : Earth
+    {
+        //string name;
+        //int area;
+        int quantityOfLands;
+        public Continent()
+        {
+            Console.WriteLine("\nCreating continent...\n");
+            Console.Write("Enter name of continent: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter area of continent(int): ");
+            Area = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter quantity of lands(int): ");
+            quantityOfLands = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nCreated: ");
+            Info();
+        }
+        public new void Info()
+        {
+            Console.WriteLine("Continent");
+            Console.WriteLine("relief - " + relief);
+            Console.WriteLine("soil - " + soil);
+            Console.WriteLine("name - " + Name);
+            Console.WriteLine("area - " + Area);
+            Console.WriteLine("quantityOfLands - " + quantityOfLands);
+        }
+        public override string ToString()
+        {
+            return "/nContinent relief - " + relief + ", soil - " + soil + ", name - " + Name
+                + ", area - " + Area + ", quantityOfLands - " + quantityOfLands;
+        }
+    }
+
+    class Island : Earth
+    {
+        //string name;
+        //int area;
+        public Island()
+        {
+            Console.WriteLine("\nCreating island...\n");
+            Console.Write("Enter name of island: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter area of island(int): ");
+            Area = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nCreated: ");
+            Info();
+        }
+        public new void Info()
+        {
+            Console.WriteLine("Island");
+            Console.WriteLine("relief - " + relief);
+            Console.WriteLine("soil - " + soil);
+            Console.WriteLine("name - " + Name);
+            Console.WriteLine("area - " + Area);
+        }
+        public override string ToString()
+        {
+            return "/nIsland relief - " + relief + ", soil - " + soil + ", name - " + Name
+                + ", area - " + Area;
+        }
+    }
+
+    class State : Continent, IFlight, ISailing
+    {
+        //string name;
+        //int area;
+        public State()
+        {
+            Console.WriteLine("\nCreating state...\n");
+            Console.Write("Enter name of state: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter area of state(int): ");
+            Area = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nCreated: ");
+            Info();
+        }
+        public new void Info()
+        {
+            Console.WriteLine("State");
+            Console.WriteLine("relief - " + relief);
+            Console.WriteLine("soil - " + soil);
+            Console.WriteLine("name - " + Name);
+            Console.WriteLine("area - " + Area);
+        }
+        public string Time { get; set; }
+        string IFlight.To()
+        {
+            return "edge of the world by plane";
+        }
+        string ISailing.To()
+        {
+            return "edge of the world by ship";
+        }
+        public override string ToString()
+        {
+            return "/nContinent relief - " + relief + ", soil - " + soil + ", name - " + Name
+                + ", area - " + Area;
+        }
+    }
+
+    sealed class Sea : Water, IFlight, ISailing
+    {
+        string deepness;
+        public Sea()
+        {
+            Console.WriteLine("\nCreating sea...\n");
+            Console.Write("Enter name of sea: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter area of sea(int): ");
+            Area = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter deepness of sea: ");
+            deepness = Console.ReadLine();
+            Console.Write("\nCreated: ");
+            Info();
+        }
+        public new void Info()
+        {
+            Console.WriteLine("Sea");
+            Console.WriteLine("name - " + Name);
+            Console.WriteLine("area - " + Area);
+            Console.WriteLine("deepness - " + deepness);
+        }
+        public string Time { get; set; }
+        string IFlight.To()
+        {
+            return "home by plane";
+        }
+        string ISailing.To()
+        {
+            return "home by ship";
+        }
+    }
+    
+    interface IFlight
+    {
+        string Time { get; set; }
+        string To();
+    }
+    interface ISailing
+    {
+        string Time { get; set; }
+        string To();
+    }
+
+    class Printer
+    {
+        public Printer() { }
+        public virtual void IAmPrinting(IFlight a)
+        {
+            Console.WriteLine(a.GetType().ToString());
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Earth a = new Earth();
+            //Earth b = new Earth();
+
+            /*Continent continent = new Continent();
+            Console.WriteLine(continent.ToString());*/
+
+            /*State a = new State();
+            IFlight state = new State()
+            {
+                Time = "right now"
+            };
+            state = a as IFlight;*/
+
+            /*IFlight flight = new State
+            {
+                Time = "right now"
+            };
+            Console.WriteLine("Flight to " + flight.To() + " " + flight.Time);
+            ISailing sailing = new State
+            {
+                Time = "tomorrow"
+            };
+            Console.WriteLine("Sailing to " + sailing.To() + " " + sailing.Time);*/
+
+
+            /*IFlight flight = new State
+            {
+                Time = "Right now"
+            };
+            Console.WriteLine("Flight to " + flight.To() + " " + flight.Time);*/
+
+
+
+            //Water b = new Water();
+            //Continent c = new Continent();
+            //Island d = new Island();
+
+            //Console.WriteLine(a.ToString());
+            //Console.WriteLine(a.Equals(b));
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(b.GetHashCode());
+            //Console.WriteLine(a.GetType());
+
+            State state = new State();
+            Sea sea = new Sea();
+            Printer printer = new Printer();
+            IFlight[] flights = { state, sea };
+            for(int i = 0; i < 2; i++)
+            {
+                printer.IAmPrinting(flights[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
